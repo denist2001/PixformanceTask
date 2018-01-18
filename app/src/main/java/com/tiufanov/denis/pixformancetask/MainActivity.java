7ff7a63d0892006eaa100ce1612fbd54af6fb055
Droid <tufanov@gmail.com>
2018-01-18 19:26:20 +0100

    First commit. Base project from Android Studio with ViewPager

---
+++ app/src/main/java/com/tiufanov/denis/pixformancetask/MainActivity.java
@@ -0,0 +1,37 @@
+package com.tiufanov.denis.pixformancetask;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+
+import com.tiufanov.denis.pixformancetask.fragment.SectionsPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    // Create the adapter that will return a fragment for each of the two
+    // primary sections of the activity.
+    /*
+        The {@link android.support.v4.view.PagerAdapter} that will provide
+        fragments for each of the sections. We use a
+        {@link FragmentPagerAdapter} derivative, which will keep every
+        loaded fragment in memory. If this becomes too memory intensive, it
+        may be best to switch to a
+        {@link android.support.v4.app.FragmentStatePagerAdapter}.
+    */
+        SectionsPagerAdapter mSectionsPagerAdapter =
+                new SectionsPagerAdapter(getSupportFragmentManager());
+
+    // Set up the ViewPager with the sections adapter.
+    /*
+        The {@link ViewPager} that will host the section contents.
+    */
+        ViewPager mViewPager = findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+    }
+}
